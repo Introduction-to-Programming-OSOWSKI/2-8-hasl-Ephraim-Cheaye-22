d1206e25338778fdde04d19e9c8be271709d4d98
Ephraim-Cheaye-22 <833955@mystma.org>
2021-10-04 09:40:54 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    for i in range(0, len(w)):
+        print(w[i])
+        if w[i] == "l":
+            return True
+    return False
+        
+
+print(hasL("alabama"))
